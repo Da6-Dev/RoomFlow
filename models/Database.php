d2e32a230e650d504c927f9d0d15037ff5eca2d7
Davi Passos <devdapacontact@gmail.com>
2025-03-31 08:23:15 -0300

    atualizando database

--- models/Database.php
+++ models/Database.php
@@ -3,7 +3,7 @@
 class Database{
     public function getConnection(){
         try{
-            $pdo = new PDO("mysql:dbname=roomflox;host=127.0.0.1", "root", "");
+            $pdo = new PDO("mysql:dbname=roomflow;host=127.0.0.1", "root", "");
             return $pdo;
         }catch (PDOException $err){
 
